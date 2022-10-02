4e5dcf4a0fd6a107b5f85edc465d40d54943f036
Jens Gerke <jens.gerke@direktiv.io>
2022-10-02 13:21:48 +0200

    ...
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- ip-change/windows.yaml.ip.ps1
+++ ip-change/windows.yaml.ip.ps1
@@ -22,7 +22,8 @@ function Set-IP
 
     Set-NetIPInterface -InterfaceAlias $alias -Dhcp Disabled
     Remove-NetIPAddress -InterfaceAlias $alias-Confirm:$false
-    New-NetIPAddress -InterfaceAlias $alias -IPAddress $args[0] -AddressFamily IPv4 -PrefixLength 24 
+    New-NetIPAddress -InterfaceAlias $alias -IPAddress $args[0] -AddressFamily IPv4 -DefaultGateway $args[1] -PrefixLength $args[2]
+    Set-DnsClientServerAddress -InterfaceAlias $alias -serveraddresses 8.8.8.8
     
 }
 
@@ -30,6 +31,8 @@ $username = $args[0]
 $password = $args[1]
 $old = $args[2]
 $new = $args[3]
+$gw = $args[4]
+$prefix = $args[5]
 
 
 
@@ -38,7 +41,7 @@ Write-Output "login in as $username, change from $old to $new"
 $pass = ConvertTo-SecureString $password -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username,$pass)
 
-Invoke-Command -Authentication Negotiate -ComputerName $old -ScriptBlock ${function:Set-IP} -ArgumentList $new -credential $cred
+Invoke-Command -Authentication Negotiate -ComputerName $old -ScriptBlock ${function:Set-IP} -ArgumentList $new,$gw,$prefix -credential $cred
 
 
 
