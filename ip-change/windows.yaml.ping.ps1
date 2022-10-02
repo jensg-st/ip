2199a62b2a930fcd5317f4a78e5d4369359f1e52
Jens Gerke <jens.gerke@direktiv.io>
2022-10-02 17:34:54 +0200

    ...
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- ip-change/windows.yaml.ping.ps1
+++ ip-change/windows.yaml.ping.ps1
@@ -1,3 +1,14 @@
-if (-NOT (Test-Connection -ComputerName $args[0] -TimeoutSeconds 1 -Quiet)) {
-    EXIT 255
-} 
+# if (-NOT (Test-Connection -ComputerName $args[0] -TimeoutSeconds 1 -Quiet)) {
+#     EXIT 255
+# } 
+
+$username = $args[0]
+$password = $args[1]
+$new = $args[1]
+
+Write-Output "login in as $username, change from $old to $new"
+
+$pass = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username,$pass)
+
+Invoke-Command -Authentication Negotiate -ComputerName $new -ScriptBlock ${ Get-Culture }  -credential $cred
